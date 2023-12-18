6b4559544631b483e0b0618cf72ba8ac13ef9633
kamen <2026kamenskii.ka@student.letovo.ru>
2023-12-18 13:20:10 +0300

    Added ++ as multiplication and -- as division, fixed wrong output of video, added console outpiut

--- CVProject.py
+++ CVProject.py
@@ -1,28 +1,41 @@
 import mediapipe
 import cv2
 import numpy as np
-from time import sleep
+from time import time_ns
 
 cap = cv2.VideoCapture(0)
 MODEL = mediapipe.solutions.hands.Hands(max_num_hands=2)
 
-FINGER_BASES = list(range(1, 18, 4))
+EPS = 20
 
-EPS = 50
-
-GESTURE_CONFIDENCE = 5
+GESTURE_CONFIDENCE = 10
+TIMER = 3000
 
 confidence_gesture = ''
 cur_gesture = ''
 
 confidence = 0
 
+expr = ''
+
+current_time = 0
+def islower(a, b):
+    return a < b and abs(a - b) > EPS
+
+
+def ishigher(a, b):
+    return a > b and abs(a - b) > EPS
+
+
+def isequal(a, b):
+    return abs(a - b) < EPS
 
 class GestureDetector():
-    def __init__(self, landmarks, shape, ) -> None:
+    def __init__(self, landmarks, shape, hand) -> None:
         self.landmarks = landmarks
         self.shape = shape
         self.points = []
+        self.hand = hand
 
     def get_points(self):
         for mark in self.landmarks.landmark:
@@ -33,51 +46,140 @@ class GestureDetector():
         self.get_points()
 
         if (self.points[4][1] < self.points[3][1]
-            and self.points[5][1] < self.points[9][1]
-            and self.points[8][0] > self.points[7][0]
-            and self.points[12][0] > self.points[11][0]
-            and self.points[16][0] > self.points[15][0]
-            and self.points[20][0] > self.points[19][0]
-            and self.points[8][0] < self.points[0][0]
-            and self.points[12][0] < self.points[0][0]
-            and self.points[16][0] < self.points[0][0]
-            and self.points[20][0] < self.points[0][0]):
+
+                and ishigher(self.points[8][0], self.points[7][0])
+                and ishigher(self.points[12][0], self.points[11][0])
+                and ishigher(self.points[16][0], self.points[15][0])
+                and ishigher(self.points[20][0], self.points[19][0])
+                and islower(self.points[8][0], self.points[0][0])
+                and islower(self.points[12][0], self.points[0][0])
+                and islower(self.points[16][0], self.points[0][0])
+                and islower(self.points[20][0], self.points[0][0])
+                and self.points[5][1] < self.points[9][1]):
 
             return '='
+        elif abs(self.points[5][1] - self.points[8][1]) < EPS < abs(self.points[5][0] - self.points[8][0]):
+            if abs(self.points[9][1] - self.points[12][1]) < EPS < abs(
+                    self.points[9][0] - self.points[12][0]):
+                return 'backspace'
+            return '-'
+        elif (
+                islower(self.points[8][1], self.points[5][1])
+                and ishigher(self.points[12][1], self.points[10][1])
+                and ishigher(self.points[16][1], self.points[14][1])
+                and ishigher(self.points[20][1], self.points[18][1])
+        ):
+            return '1'
+        elif (
+                islower(self.points[8][1], self.points[5][1])
+                and islower(self.points[12][1], self.points[9][1])
+                and ishigher(self.points[16][1], self.points[14][1])
+                and ishigher(self.points[20][1], self.points[18][1])
+        ):
+            return '2'
+        elif (
+                islower(self.points[8][1], self.points[5][1])
+                and islower(self.points[12][1], self.points[9][1])
+                and islower(self.points[16][1], self.points[13][1])
+                and ishigher(self.points[20][1], self.points[18][1])
+        ):
+            return '3'
+        elif (
+                islower(self.points[8][1], self.points[5][1])
+                and islower(self.points[12][1], self.points[9][1])
+                and islower(self.points[16][1], self.points[13][1])
+                and islower(self.points[20][1], self.points[17][1])
 
-        cnt = 0
-        for base in FINGER_BASES:
-            if self.points[base][1] - EPS > self.points[base + 3][1]:
-                cnt += 1
-                if base == 1 and abs(self.points[base][0] - self.points[base + 3][0]) > 1.5 * EPS:
-                    cnt -= 1
+        ):
 
-        return str(cnt)
+            if 'Left' in str(self.hand):
+                if self.points[4][0] < self.points[1][0]:
+                    return '4'
+                else:
+                    return '5'
+            else:
+                if self.points[4][0] > self.points[1][0]:
+                    return '4'
+                else:
+                    return '5'
+        elif (
+                ishigher(self.points[8][1], self.points[6][1])
+                and ishigher(self.points[12][1], self.points[10][1])
+                and ishigher(self.points[16][1], self.points[14][1])
+                and ishigher(self.points[20][1], self.points[18][1])
+        ):
+            return '0'
+        return 'NONE'
 
-
+cnt = 0
 while cap.isOpened():
     ret, img = cap.read()
     if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
         break
     img = np.fliplr(img)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
     results = MODEL.process(img)
     if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            detector = GestureDetector(hand_landmarks, img.shape)
-            cur_gesture += detector.rechognize()
-        if '=' in cur_gesture:
-            cur_gesture = '='
+        for i, hand_landmarks in enumerate(results.multi_hand_landmarks):
+            x_base = int(hand_landmarks.landmark[0].x * img.shape[1])
+            y_base = int(hand_landmarks.landmark[0].y * img.shape[0])
+            cv2.circle(img, (x_base, y_base), radius=10, color=(0, 0, 255), thickness=-1)
 
-        if cur_gesture != confidence_gesture:
-            confidence_gesture = cur_gesture
-            confidence = 1
-        else:
-            confidence += 1
 
-        if confidence >= GESTURE_CONFIDENCE:
-            print(cur_gesture, 'CONFIDENT')
+    if time_ns()//1000000 - current_time > TIMER:
 
-        cur_gesture = ''
+        results = MODEL.process(img)
+        if results.multi_hand_landmarks:
+            for i, hand_landmarks in enumerate(results.multi_hand_landmarks):
+                detector = GestureDetector(hand_landmarks, img.shape, results.multi_handedness[i])
+                cur_gesture += detector.rechognize()
 
+            if cur_gesture != '':
+                if 'NONE' in cur_gesture:
+                    cur_gesture = ''
+                if '1' in cur_gesture and '-' in cur_gesture:
+                    cur_gesture = '+'
+                elif '-' in cur_gesture:
+                    cur_gesture = '-'
+                elif '=' in cur_gesture:
+                    cur_gesture = '='
+                elif cur_gesture.isdigit():
+                    cur_gesture = sum(list(map(int, list(cur_gesture))))
+                    if cur_gesture == 10:
+                        cur_gesture = '0'
+                if cur_gesture != confidence_gesture:
+                    confidence_gesture = cur_gesture
+                    confidence = 1
+                else:
+                    confidence += 1
+
+                print('detecting finished... gesture rechognized as', cur_gesture, 'confidence:', confidence, '/', GESTURE_CONFIDENCE)
+
+                if confidence >= GESTURE_CONFIDENCE:
+                    print('proceed')
+                    confidence_gesture = ''
+                    confidence = 0
+                    if cur_gesture == '=':
+                        print('your expression was', expr, 'your result is', eval(expr))
+                        cur_gesture = ''
+                        expr = ''
+
+                    elif cur_gesture == 'backspace':
+                        expr = expr[:-1]
+                    else:
+                        expr += str(cur_gesture)
+                        try:
+                            if expr[-1] == expr[-2] == '+':
+                                expr = expr[:-2]+'*'
+                            elif expr[-1] == expr[-2] == '-':
+                                expr = expr[:-2]+'/'
+                        except IndexError:
+                            pass
+
+                    current_time = time_ns()//1000000
+                cur_gesture = ''
+            else:
+                print('unknown gesture')
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     cv2.imshow("Hands", img)
